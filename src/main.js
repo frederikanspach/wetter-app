882de17d3e5e110a4488bba767adc286886ee1f0
Frederik Anspach <frederik.anspach@web.de>
2026-02-17 00:18:39 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import "./scss/main.scss";
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Wetter App 2026 geladen");
+  // API-Calls
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const scrollList = document.querySelector(".today-forecast__list");
+
+  if (scrollList) {
+    scrollList.addEventListener("wheel", (event) => {
+      if (event.deltaY !== 0) {
+        event.preventDefault();
+        scrollList.scrollLeft += event.deltaY;
+      }
+    });
+  }
+});
